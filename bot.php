74ea829ef834ae31b97c350b36796df0d8807270
Kuson23938 <95063696+Kuson23938@users.noreply.github.com>
2022-08-07 21:34:29 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,17 +1,44 @@
-// URL API LINE
+<?php
+
+
 $API_URL = 'https://api.line.me/v2/bot/message';
-// ใส่ Channel access token (long-lived)
-$ACCESS_TOKEN = '9OyE6AL14J0JAlGFPmaZwBot2ieTmTQ7sehJ4VAuUyVeCR/Rb3goghXxVMsOwCsvQjXXNu6Q8fx7LYiCq+CO2rvTVmHvX0M4oA5U8eJs10TSnHj6YQQUmyk9XtvEGdAw1bzrBLboQBdT3OvmMjrZBwdB04t89/1O/w1cDnyilFU=';
-// ใส่ Channel Secret
-$CHANNEL_SECRET = '9cc369a589e4c8b1ad0e6f8cfc09b7b2';
-//--------------------------------------------------------------
-// Set HEADER
+$ACCESS_TOKEN = '9OyE6AL14J0JAlGFPmaZwBot2ieTmTQ7sehJ4VAuUyVeCR/Rb3goghXxVMsOwCsvQjXXNu6Q8fx7LYiCq+CO2rvTVmHvX0M4oA5U8eJs10TSnHj6YQQUmyk9XtvEGdAw1bzrBLboQBdT3OvmMjrZBwdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = '9cc369a589e4c8b1ad0e6f8cfc09b7b2';
+
+
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
-// Get request content
-$request = file_get_contents('php://input');
-// Decode JSON to Array
-$request_array = json_decode($request, true);
-//---------------------------------------------------------------
+
+$request = file_get_contents('php://input');   // Get request content
+$request_array = json_decode($request, true);   // Decode JSON to Array
+
+
+
+if ( sizeof($request_array['events']) > 0 ) {
+
+    foreach ($request_array['events'] as $event) {
+
+        $reply_message = '';
+        $reply_token = $event['replyToken'];
+
+
+        $data = [
+            'replyToken' => $reply_token,
+            'messages' => [['type' => 'text', 'text' => json_encode($request_array)]]
+        ];
+        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
+
+        echo "Result: ".$send_result."\r\n";
+        
+    }
+}
+
+echo "OK";
+
+
+
+
 function send_reply_message($url, $post_header, $post_body)
 {
     $ch = curl_init($url);
@@ -25,22 +52,5 @@ function send_reply_message($url, $post_header, $post_body)
 
     return $result;
 }
-//-------------------------------------------------------------
-if ( sizeof($request_array['events']) > 0 ) {
-      foreach ($request_array['events'] as $event) {
-      
-      $reply_message = '';
-      $reply_token = $event['replyToken'];
-      $data = [
-         'replyToken' => $reply_token,
-         'messages' => [
-            ['type' => 'text', 
-             'text' => json_encode($request_array)]
-         ]
-      ];
-      $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-      $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-      echo "Result: ".$send_result."\r\n";
-   }
-}
-echo "OK";
+
+?>
