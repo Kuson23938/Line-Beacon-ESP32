f78116380627f1d0f7f4344c58031f3f6b9b7c5f
Kuson23938 <95063696+Kuson23938@users.noreply.github.com>
2022-08-08 20:09:09 +0700

    Update beacon.php

--- beacon.php
+++ beacon.php
@@ -12,167 +12,68 @@ $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 var_export($request_array);
 
-$jsonFlex = [
-    "type" => "flex",
-    "altText" => "Hello Flex Message",
-    "contents" => [
-      "type" => "bubble",
-      "direction" => "ltr",
-      "header" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "Purchase",
-            "size" => "lg",
-            "align" => "start",
-            "weight" => "bold",
-            "color" => "#009813"
-          ],
-          [
-            "type" => "text",
-            "text" => "฿ 100.00",
-            "size" => "3xl",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "Rabbit Line Pay",
-            "size" => "lg",
-            "weight" => "bold",
-            "color" => "#000000"
-          ],
-          [
-            "type" => "text",
-            "text" => "2019.02.14 21:47 (GMT+0700)",
-            "size" => "xs",
-            "color" => "#B2B2B2"
-          ],
-          [
-            "type" => "text",
-            "text" => "Payment complete.",
-            "margin" => "lg",
-            "size" => "lg",
-            "color" => "#000000"
-          ]
-        ]
-      ],
-      "body" => [
-        "type" => "box",
-        "layout" => "vertical",
-        "contents" => [
-          [
-            "type" => "separator",
-            "color" => "#C3C3C3"
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "Merchant",
-                "align" => "start",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => "BTS 01",
-                "align" => "end",
-                "color" => "#000000"
-              ]
-            ]
-          ],
-          [
-            "type" => "box",
-            "layout" => "baseline",
-            "margin" => "lg",
-            "contents" => [
-              [
-                "type" => "text",
-                "text" => "New balance",
-                "color" => "#C3C3C3"
-              ],
-              [
-                "type" => "text",
-                "text" => "฿ 45.57",
-                "align" => "end"
-              ]
-            ]
-          ],
-          [
-            "type" => "separator",
-            "margin" => "lg",
-            "color" => "#C3C3C3"
-          ]
-        ]
-      ],
-      "footer" => [
-        "type" => "box",
-        "layout" => "horizontal",
-        "contents" => [
-          [
-            "type" => "text",
-            "text" => "View Details",
-            "size" => "lg",
-            "align" => "start",
-            "color" => "#0084B6",
-            "action" => [
-              "type" => "uri",
-              "label" => "View Details",
-              "uri" => "https://google.co.th/"
-            ]
-          ]
-        ]
-      ]
-    ]
-  ];
+$nowtimespc = date("H:i:s");
+  $nowdatespc = date("Y-m-d");
+  $datetimemixspc = strtotime($nowdatespc.' '.$nowtimespc);
+  $dtmixspcinsert = date('Y-m-d H:i:s', $datetimemixspc);
+  $LINEData = file_get_contents('php://input');
+  $jsonData = json_decode($LINEData,true);
+  $replyToken = $jsonData["events"][0]["replyToken"];
+  $text = $jsonData["events"][0]["message"]["text"];
+  $userId = $jsonData["events"][0]["source"]['userId'];
+  $timestamp = $jsonData["events"][0]["timestamp"];
+  $type = $jsonData["events"][0]["message"]['type'];
+  $beacon_type = $jsonData["events"][0]["beacon"]['type'];
+  $beacon_hwid = $jsonData["events"][0]["beacon"]['hwid'];
+  
+  $curl = curl_init();
 
 
-
-if ( sizeof($request_array['events']) > 0 ) {
-    foreach ($request_array['events'] as $event) {
-        error_log(json_encode($event));
-        $reply_message = '';
-        $reply_token = $event['replyToken'];
-
-
-        $data = [
-            'replyToken' => $reply_token,
-            'messages' => [$jsonFlex]
-        ];
-
-        print_r($data);
-
-        $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-        $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
-
-        echo "Result: ".$send_result."\r\n";
-        
-    }
-}
-
-echo "OK";
-
-
-
-
-function send_reply_message($url, $post_header, $post_body)
-{
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    $result = curl_exec($ch);
-    curl_close($ch);
-
+  function sendMessage($replyJson, $token){
+          $ch = curl_init($token["URL"]);
+          curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+          curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+          curl_setopt($ch, CURLOPT_POST, true);
+          curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+              'Content-Type: application/json',
+              'Authorization: Bearer ' . $token["AccessToken"])
+              );
+          curl_setopt($ch, CURLOPT_POSTFIELDS, $replyJson);
+          $result = curl_exec($ch);
+          curl_close($ch);
     return $result;
-}
+  }
+  
+  if($type="beacon"){
+      if($beacon_type=="enter"){
+        
+          
+          $sqlmember = "SELECT `member_userid`, `member_name` FROM `data_member` WHERE `member_userid` = '$userId'";
+          $ressqlmember = $connectdb->query($sqlmember);
+          $row = mysqli_fetch_assoc($ressqlmember);
 
+          $member_name = $row['member_name'];
+          $sql = "INSERT INTO `savedata`(`beacon_userid`,`beacon_hwid`,beacon_time) VALUES('$userId','$beacon_hwid','$dtmixspcinsert')";
+          $res = $connectdb->query($sql);
+          
+          $message = '{
+            "type": "text",
+            "text": "สวัสดีคุณ '.$member_name.'"
+            }';
+          $replymessage = json_decode($message);
+      }
+     
+  }
+
+
+
+  $lineData['URL'] = "https://api.line.me/v2/bot/message/reply";
+  $lineData['AccessToken'] = "9OyE6AL14J0JAlGFPmaZwBot2ieTmTQ7sehJ4VAuUyVeCR/Rb3goghXxVMsOwCsvQjXXNu6Q8fx7LYiCq+CO2rvTVmHvX0M4oA5U8eJs10TSnHj6YQQUmyk9XtvEGdAw1bzrBLboQBdT3OvmMjrZBwdB04t89/1O/w1cDnyilFU=";
+  $replyJson["replyToken"] = $replyToken;
+  $replyJson["messages"][0] = $replymessage;
+
+  $encodeJson = json_encode($replyJson);
+  $results = sendMessage($encodeJson,$lineData);
+
+  http_response_code(200);
 ?>
